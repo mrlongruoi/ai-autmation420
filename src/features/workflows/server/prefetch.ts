9650c6630b931860ce76711ef0416e16305ca17e
mrlongruoi <mrlong01012020@gmail.com>
2025-10-16 01:22:22 +0700

    14: Workflow Page

--- src/features/workflows/server/prefetch.ts
+++ src/features/workflows/server/prefetch.ts
@@ -7,6 +7,11 @@ type Input = inferInput<typeof trpc.workflows.getMany>;
  * Prefetch all workflows
  */
 export const prefetchWorkflows = (params: Input) => {
-    
   return prefetch(trpc.workflows.getMany.queryOptions(params));
+};
+/**
+ * Prefetch a single workflow
+ */
+export const prefetchWorkflow = (id: string) => {
+  return prefetch(trpc.workflows.getOne.queryOptions({ id }));
 };
